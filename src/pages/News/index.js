a9f6abb42c8f0f64cc83dd2c550843bb50e229e1
Nguyen Thanh Dat <thuykhangvn@gmail.com>
2023-05-30 20:27:39 +0700

    update

--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -16,8 +16,8 @@ export default function News() {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 2,
           infinite: true,
           dots: true,
         },
@@ -25,9 +25,9 @@ export default function News() {
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
         },
       },
       {
